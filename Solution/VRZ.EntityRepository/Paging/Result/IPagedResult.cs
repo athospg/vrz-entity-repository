93448e4ae5824b35d01d8ad63e0eef62803ff5c6
Athos Garcia <athospg@outlook.com>
2020-12-06 23:24:53 -0300

    Add paged result interface/class and fix pipeline dotnet version

---
+++ Solution/VRZ.EntityRepository/Paging/Result/IPagedResult.cs
@@ -0,0 +1,18 @@
+﻿namespace VRZ.EntityRepository.Paging.Result
+{
+    public interface IPagedResult
+    {
+        public int PageSize { get; set; }
+
+        public int TotalCount { get; set; }
+
+        public int CurrentPage { get; set; }
+
+        public int TotalPages { get; set; }
+
+
+        public bool HasPrevious => CurrentPage > 1;
+
+        public bool HasNext => CurrentPage < TotalPages;
+    }
+}
