5848eecf8bee7e03ed1148a07b4055b2e0d549a6
Athos Garcia <athospg@outlook.com>
2020-12-06 23:28:08 -0300

    Merge PR #12: Add paged result interface/class and fix pipeline dotnet version

--- Solution/VRZ.EntityRepository/Paging/PagedList.cs
+++ Solution/VRZ.EntityRepository/Paging/PagedList.cs
@@ -1,23 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using VRZ.EntityRepository.Paging.Result;
 
 namespace VRZ.EntityRepository.Paging
 {
-    public class PagedList<T> : List<T>
+    public class PagedList<T> : List<T>, IPagedResult
     {
-        public int PageSize { get; }
+        public int PageSize { get; set; }
 
-        public int TotalCount { get; }
+        public int TotalCount { get; set; }
 
-        public int CurrentPage { get; }
+        public int CurrentPage { get; set; }
 
-        public int TotalPages { get; }
+        public int TotalPages { get; set; }
 
 
         public bool HasPrevious => CurrentPage > 1;
 
         public bool HasNext => CurrentPage < TotalPages;
 
+
+        public PagedList()
+        {
+        }
 
         public PagedList(IEnumerable<T> items, int count, int pageNumber, int pageSize)
         {
